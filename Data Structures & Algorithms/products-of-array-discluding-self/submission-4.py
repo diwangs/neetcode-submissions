bdef0cda02b86ecb43dc7b07bf30a851e441ed56
diwangs <27280516+diwangs@users.noreply.github.com>
2026-09-21 02:30:00 -0400

    Add: products-of-array-discluding-self - submission-4

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-4.py
@@ -0,0 +1,52 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        products = [1] * len(nums)
+
+        prefix = 1
+        for i in range(len(nums)):
+            products[i] *= prefix
+            prefix *= nums[i]
+
+        suffix = 1
+        for i in range(len(nums) - 1, -1, -1):
+            products[i] *= suffix
+            suffix *= nums[i]
+
+        return products
+        
+    def productExceptSelfY(self, nums: List[int]) -> List[int]:
+        for i in range(len(nums) - 1):
+            # if i == 0:
+            #     prefix_product.append(num)
+            # else:
+            prefix_product.append(nums[i] * prefix_product[-1])
+        # prefix_product.pop()
+        # prefix_product.insert(0, 1)
+
+        suffix_product = [1]
+        for i in range(len(nums) - 1, 0, -1):
+            # if i == len(nums) - 1:
+            #     suffix_product.insert(0, nums[i])
+            # else:
+            suffix_product.insert(0, nums[i] * suffix_product[0])
+        # suffix_product.pop(0)
+        # suffix_product.append(1)
+
+        return [ prefix_product[i] * suffix_product[i] for i in range(len(nums)) ]
+
+    def productExceptSelfZ(self, nums: List[int]) -> List[int]:
+        res = [1] * (len(nums))
+
+        prefix = 1
+        for i in range(len(nums)):
+            res[i] = prefix
+            prefix *= nums[i]
+        postfix = 1
+        for i in range(len(nums) - 1, -1, -1):
+            res[i] *= postfix
+            postfix *= nums[i]
+        return res
+
+        
+
+            
